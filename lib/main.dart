3a5cd340e897099847f6e290f0f5f74d0539523f
mzunohkaru <winkaisyu@gmail.com>
2024-02-13 22:36:26 +0900

    first deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_profile_web/layout/responsive_widget.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(SidebarXExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'URL Launcher',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'URL Launcher'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  Future<void> launchInBrowser(Uri url) async {
-    if (!await launchUrl(
-      url,
-      mode: LaunchMode.externalApplication,
-      webOnlyWindowName: '_blank',
-    )) {
-      // エラーを投げる代わりに、ユーザーにエラーメッセージを表示する
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Could not launch $url')),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              launchInBrowser(Uri.parse('https://flutter.dev'));
-            },
-            child: Text('Open Flutter Website'),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'URL Launcher',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'URL Launcher'),
+//     );
+//   }
+// }
