c63a91b1fc8f61178fe7dc357bd4bd713ee5bb7e
mzunohkaru <winkaisyu@gmail.com>
2024-02-13 21:43:27 +0900

    first deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_profile_web/widget/subtitle_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -46,24 +47,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SubtitleText(subtitle: "ERROR"),
             const Text(
               'You have pushed the button this many times:',
             ),
---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromARGB(255, 0, 0, 0);
+const canvasColor = Color.fromARGB(255, 40, 40, 40);
+const scaffoldBackgroundColor = Colors.black87;
+const accentCanvasColor = Color.fromARGB(255, 101, 101, 106);
+final actionColor = Color.fromARGB(255, 125, 125, 136).withOpacity(0.6);
+final divider = Divider(color: Colors.white.withOpacity(0.3), height: 1);
